280bcbde1243723c8ad74c687cc9654d2e8448ff
Balamahendira <balamahendhira@gmail.com>
2022-08-16 19:15:00 +0530

    final edit

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
   styleUrls: ['./success.component.css']
 })
 export class SuccessComponent implements OnInit {
-
+edited = false;
   constructor() { }
 
   ngOnInit(): void {
